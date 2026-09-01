c7ba0ede0d23168d11cb0daa7fdabdd0a8b28b97
nikrut <40463639+nikrut@users.noreply.github.com>
2026-09-01 22:54:41 +0300

    Add src/index.ts

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export * from "./backtest.js";
+export * from "./encoder.js";
+export * from "./planner.js";
+export * from "./risk.js";
+export * from "./strategies/mean-reversion.js";
+export * from "./strategies/momentum.js";
+export * from "./types.js";
